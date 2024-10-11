520dc23865f705b8767336ee343b5e4857faa37a
Soraya Taraszka <soraya@unitdigital.co.uk>
2024-10-11 14:12:25 +0100

    display assignee

--- app/Livewire/ListTickets.php
+++ app/Livewire/ListTickets.php
@@ -13,11 +13,15 @@ class ListTickets extends Component
 
         $tickets = Tickets::get()->map( function($ticket){
             $assigner = User::find($ticket->assigner);
+            $assignee = User::find($ticket->assignee);
             return [
                 'id' => $ticket->id,
                 'title' => $ticket->title,
                 'assigner' => [
                     'name' => $assigner->name,
+                ],
+                'assignee' => [
+                    'name' => $assignee->name,
                 ]
             ];
         });
--- resources/views/livewire/list-tickets.blade.php
+++ resources/views/livewire/list-tickets.blade.php
@@ -3,6 +3,7 @@
        <li>
             <span>{{$ticket['title']}}</span>
             <span>{{$ticket['assigner']['name']}}</span>
+            <span>{{$ticket['assignee']['name']}}</span>
         </li>
     @endforeach
 </div>
