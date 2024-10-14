8d7120046fdf413701deb16da9d2780b2d6ede39
Soraya Taraszka <soraya@unitdigital.co.uk>
2024-10-14 09:10:14 +0100

    install and implement tailwind

--- resources/views/livewire/list-tickets.blade.php
+++ resources/views/livewire/list-tickets.blade.php
@@ -1,8 +1,15 @@
-<ul>
+<ul >
+    <li class="grid grid-cols-5 gap-4 odd:bg-[#fca5a5]"">
+        <span class="w-[500px]">Title</span>
+        <span>Status</span>
+        <span>Priority</span>
+        <span>Assigner</span>
+        <span>Assignee</span>
+    </li>
     @foreach( $tickets as $ticket)
-       <li>
-            <span>{{$ticket['title']}}</span>
-            <span>{{$ticket['status']}}</span>
+       <li class="grid grid-cols-5 gap-4 odd:bg-[#fca5a5]"">
+            <span class="w-[500px]">{{$ticket['title']}}</span>
+            <span class="w-10">{{$ticket['status']}}</span>
             <span>{{$ticket['priority']}}</span>
             <span>{{$ticket['assigner']['name']}}</span>
             <span>{{$ticket['assignee']['name']}}</span>
