44f585f3f0953da70b3b87dfb5e47eca36d9be63
Soraya Taraszka <soraya@unitdigital.co.uk>
2024-10-14 15:53:09 +0100

    add bold to list

--- resources/views/livewire/list-tickets.blade.php
+++ resources/views/livewire/list-tickets.blade.php
@@ -11,9 +11,9 @@
     @foreach( $tickets as $ticket)
        <li class=" odd:bg-[#fca5a5] "">
             <div class="max-w-screen-xl mx-auto grid grid-cols-5 gap-4">
-                <a href="/ticket/{{$ticket['id']}}" class=" p-2">{{$ticket['title']}}</a>
-                <a href="/ticket/{{$ticket['id']}}" class=" p-2">{{$ticket['status']}}</a>
-                <a href="/ticket/{{$ticket['id']}}" class="p-2">{{$ticket['priority']}}</a>
+                <a href="/ticket/{{$ticket['id']}}" class=" p-2 capitalize">{{$ticket['title']}}</a>
+                <a href="/ticket/{{$ticket['id']}}" class=" p-2 uppercase font-bold">{{$ticket['status']}}</a>
+                <a href="/ticket/{{$ticket['id']}}" class="p-2 uppercase font-bold">{{$ticket['priority']}}</a>
                 <a href="/ticket/{{$ticket['id']}}" class="p-2">{{$ticket['assigner']['name']}}</a>
                 <a href="/ticket/{{$ticket['id']}}" class="p-2">{{$ticket['assignee']['name']}}</a>
             </div>
