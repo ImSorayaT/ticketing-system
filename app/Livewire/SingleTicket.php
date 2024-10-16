a54a7347557601e90a462558522331b8c954bdae
Soraya Taraszka <soraya@unitdigital.co.uk>
2024-10-16 16:09:53 +0100

    update priority and state of a ticket

--- app/Livewire/SingleTicket.php
+++ app/Livewire/SingleTicket.php
@@ -5,26 +5,40 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\Tickets;
 use App\Models\User;
+use App\Http\Requests\UpdateTicketsRequest;
  
 class SingleTicket extends Component
 {
 
     public $id;
+    public $ticket;
+    public $ticketStatus = '';
+    public $ticketPriority = '';
 
     public function mount($id)
     {
         $this->id = $id;
+        $this->ticket = Tickets::findOrFail($id);
+        $this->ticketStatus = $this->ticket->status;
+        $this->ticketPriority = $this->ticket->priority;
     }
 
+    public function update( ){
+        $this->ticket->update(
+            [
+                'status' => $this->ticketStatus,
+                'priority' => $this->ticketPriority,
+            ]
+        );
+    }
 
     public function render()
     {
-        $ticket = Tickets::findOrFail($this->id);
+        $ticket = $this->ticket;
         $assigner = User::find($ticket->assigner);
         $assignee = User::find($ticket->assignee);
 
         $ticketArray = [
-            
                 'id' => $ticket->id,
                 'title' => $ticket->title,
                 'status' => $ticket->status,
--- resources/views/livewire/single-ticket.blade.php
+++ resources/views/livewire/single-ticket.blade.php
@@ -1,11 +1,26 @@
 <div>
-    <h1 class="text-4xl">{{ $ticketArray['title']}}</h1>
-    <div class="text-sm mb-4 mt-6 space-y-1">
-        <p class="flex gap-4"><span class="basis-16 ">Status:</span> <span class="uppercase font-bold">{{ $ticketArray['status']}}</span></p>
-        <p class="flex gap-4"><span class="basis-16 ">Priority:</span> <span class="uppercase font-bold">{{ $ticketArray['priority']}}</span></p>
+    <h1 class="text-4xl mb-6">{{ $ticketArray['title']}}</h1>
+    <form class="text-sm mb-4 space-y-1" wire:submit="update">
+        <input type="submit" class="bg-zinc-400 block p-2 mb-5 rounded-xl text-white cursor-pointer" value="Update Ticket">
+        <p class="flex gap-4">
+            <span class="basis-16 ">Status:</span> 
+            <select class="uppercase font-bold" wire:model="ticketStatus" name="ticketStatus">
+                @foreach(App\Enum\TicketStatus::cases() as $option)
+                    <option value="{{ $option->value }}">{{ $option->value }}</option>
+                @endforeach 
+            </select>
+        </p>
+        <p class="flex gap-4">
+            <span class="basis-16 ">Priority:</span> 
+            <select class="uppercase font-bold" wire:model="ticketPriority" name="ticketPriority">
+                @foreach(App\Enum\TicketPriority::cases() as $option)
+                    <option value="{{ $option->value }}">{{ $option->value }}</option>
+                @endforeach
+            </select>
+        </p>
         <p class="flex gap-4"><span class="basis-16 ">Assigner:</span> {{ $ticketArray['assigner']['name']}}</p>
         <p class="flex gap-4"><span class="basis-16 ">Assignee:</span> {{ $ticketArray['assignee']['name']}}</p>
-    </div>
+    </form>
 
     <p> {{ $ticketArray['content']}}</p>
 </div>
