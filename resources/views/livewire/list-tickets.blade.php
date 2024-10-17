bc73135b084e50d644c1b1e5023961d6d1af762c
Soraya Taraszka <soraya@unitdigital.co.uk>
2024-10-17 15:14:57 +0100

    fix table styling

--- resources/views/livewire/list-tickets.blade.php
+++ resources/views/livewire/list-tickets.blade.php
@@ -1,21 +1,21 @@
 <ul >
     <li class=""">
-        <div class="max-w-screen-xl mx-auto grid grid-cols-5 gap-4">
-            <span class=" p-2">Title</span>
-            <span class="p-2">Status</span>
-            <span class="p-2">Priority</span>
-            <span class="p-2">Assigner</span>
-            <span class="p-2">Assignee</span>
+        <div class="max-w-screen-xl mx-auto flex">
+            <span class=" p-2 basis-auto grow">Title</span>
+            <span class="p-2 basis-36">Status</span>
+            <span class="p-2 basis-36">Priority</span>
+            <span class="p-2 basis-36">Assigner</span>
+            <span class="p-2 basis-36">Assignee</span>
         </div>
     </li>
     @foreach( $tickets as $ticket)
-       <li class=" even:bg-[#424445] "">
-            <div class="max-w-screen-xl mx-auto grid grid-cols-5 gap-4">
-                <a href="/ticket/{{$ticket['id']}}" class=" p-2 capitalize">{{$ticket['title']}}</a>
-                <a href="/ticket/{{$ticket['id']}}" class=" p-2 uppercase font-bold">{{$ticket['status']}}</a>
-                <a href="/ticket/{{$ticket['id']}}" class="p-2 uppercase font-bold">{{$ticket['priority']}}</a>
-                <a href="/ticket/{{$ticket['id']}}" class="p-2">{{$ticket['assigner']['name']}}</a>
-                <a href="/ticket/{{$ticket['id']}}" class="p-2">{{$ticket['assignee']['name']}}</a>
+       <li class=" even:bg-[#3e3735] "">
+            <div class="max-w-screen-xl mx-auto flex">
+                <a href="/ticket/{{$ticket['id']}}" class=" p-2 capitalize basis-auto grow">{{$ticket['title']}}</a>
+                <a href="/ticket/{{$ticket['id']}}" class=" p-2 uppercase font-bold basis-36 ">{{$ticket['status']}}</a>
+                <a href="/ticket/{{$ticket['id']}}" class="p-2 uppercase font-bold basis-36 ">{{$ticket['priority']}}</a>
+                <a href="/ticket/{{$ticket['id']}}" class="p-2 basis-36 ">{{$ticket['assigner']['name']}}</a>
+                <a href="/ticket/{{$ticket['id']}}" class="p-2 basis-36 ">{{$ticket['assignee']['name']}}</a>
             </div>
         </li>
     @endforeach
--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,7 +1,7 @@
 
     <nav class="	">
-        <img width="60px" src="https://unitdigital.co.uk/wp-content/uploads/2022/03/unit-coral.png">
-        <ul>
+        <a href="/"><img width="60px" src="https://unitdigital.co.uk/wp-content/uploads/2022/03/unit-coral.png"></a>
+        <ul class="mt-6">
             <li><a href="/">Tickets</a></li>
         </ul>
     </nav>
