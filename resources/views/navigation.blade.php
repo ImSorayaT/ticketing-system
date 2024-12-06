7a625d552a778deb493afcf0aec1fde86b40a3d3
Soraya Taraszka <soraya@unitdigital.co.uk>
2024-12-06 15:37:37 +0000

    sidebar logo larger

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,6 +1,6 @@
 
     <nav class="	">
-        <a href="/"><img width="60px" src="https://unitdigital.co.uk/wp-content/uploads/2022/03/unit-coral.png"></a>
+        <a href="/"><img width="80px" src="https://unitdigital.co.uk/wp-content/uploads/2022/03/unit-coral.png"></a>
         <ul class="mt-10 flex flex-col gap-6">
             <li>
                 <a href="/" class="flex gap-2 items-center">
